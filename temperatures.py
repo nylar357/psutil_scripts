cbde3f07dabda14464aeb913f0aeab229f42f1b3
CͨoͦNᴛⷮRͬAͣ <37067686+nylar357@users.noreply.github.com>
2024-12-03 01:00:46 -0600

    Update temperatures.py

--- temperatures.py
+++ temperatures.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-# Copyright (c) 2009, Giampaolo Rodola'. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
 
-"""A clone of 'sensors' utility on Linux printing hardware temperatures.
+"""A clone of 'sensors' utility on Linux printing hardware temperatures
+"""by 
+"""█▄░█   █▄█   █░░   ▄▀█   █▀█
+"""█░▀█   ░█░   █▄▄   █▀█   █▀▄
 
 $ python3 scripts/sensors.py
 asus
